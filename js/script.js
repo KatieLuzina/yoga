ba54b5c7b68a7aa519903c3d215866b9bd4edf0f
KatieLuzina <39306742+KatieLuzina@users.noreply.github.com>
2020-05-06 19:35:05 +0500

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -35,3 +35,52 @@ window.addEventListener('DOMContentLoaded', function () {
         }
     })
 })
+
+//Timer
+
+let  deadline = '2019-10-21';
+
+function getTimeRemaining(endtime) {
+    let t = Date.parse(endtime) - Date.parse(new Date()),
+        seconds = Math.floor((t/1000) % 60),
+        minutes = Math.floor((t/1000/60) % 60), 
+        hours = Math.floor(t/(1000*60*60*60));
+
+        if (t <= 0) {
+            seconds = 0,
+            minutes = 0, 
+            hours = 0;
+        }
+        if (seconds < 10) seconds = '0' + seconds;
+        if (minutes < 10) minutes = '0' + minutes;
+        if (hours < 10) hours = '0' + hours;
+
+        return {
+            'total' : t,
+            'hours' : hours,
+            'minutes' : minutes,
+            'seconds' : seconds
+        };
+
+}
+
+function setClock(id, endtime) {
+    let timer = document.getElementById(id),
+        hours = timer.querySelector('.hours'),
+        minutes = timer.querySelector('.minutes'),
+        seconds = timer.querySelector('.seconds');
+    timeInterval = setInterval(updateClock, 1000);
+    
+    function updateClock() {
+        let t = getTimeRemaining(endtime);
+        hours.textContent = t.hours;
+        minutes.textContent = t.minutes;
+        seconds.textContent = t.seconds;  
+        
+        if (t.total <= 0) {
+            clearInterval(timeInterval);
+        }
+    }
+}
+
+setClock('timer', deadline);
